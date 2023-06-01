1a35ef58c05a368b0cb9d187b7229237c7a37afb
Đỗ Thành Nhân <124018743+thanhnhan3286@users.noreply.github.com>
2023-06-01 22:29:21 +0700

    ss10 2

--- ss10-Session-Cookie/GioHang/src/main/java/com/example/giohang/controller/ProductsController.java
+++ ss10-Session-Cookie/GioHang/src/main/java/com/example/giohang/controller/ProductsController.java
@@ -6,10 +6,7 @@ import com.example.giohang.service.IProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.LinkedHashMap;
@@ -26,7 +23,7 @@ public class ProductsController {
     @GetMapping("/")
     public String list(Model model){
         List<Products> productsList = productsService.findAll();
-        model.addAttribute("listProduct",productsList);
+        model.addAttribute("productsList",productsList);
         return "/listProduct";
     }
     @ModelAttribute("cart")
@@ -42,17 +39,39 @@ public class ProductsController {
     @GetMapping("/{id}/addToCart")
     public String addToCart(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes,
                             @ModelAttribute("cart") Map<Products, Integer> productList) {
-        Products flower = productsService.findById(id);
-        if (!cartService.checkExist(flower, productList)) {
-            productList.put(flower, 1);
+        Products products = productsService.findById(id);
+        Integer quantity;
+        if (!cartService.checkExist(products, productList)) {
+            productList.put(products, 1);
         } else {
-            Map.Entry<Products,Integer> m = cartService.productIntegerEntry(flower,productList);
-
-            Integer quantity = m.getValue() + 1;
+            Map.Entry<Products,Integer> m = cartService.productIntegerEntry(products,productList);
+            quantity = m.getValue() + 1;
             productList.replace(m.getKey(),quantity);
         }
-        redirectAttributes.addFlashAttribute("mess", "ADDED");
-
-        return "redirect:/";
+        return "redirect:/cart";
+    }
+    @GetMapping("/{id}/removeToCart")
+    public String removeToCart(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes,
+                               @ModelAttribute("cart") Map<Products, Integer> productList) {
+        Products products = productsService.findById(id);
+        Integer quantity;
+        if (!cartService.checkExist(products, productList)) {
+            productList.put(products, 1);
+        } else {
+            Map.Entry<Products,Integer> m = cartService.productIntegerEntry(products,productList);
+            quantity = m.getValue() - 1;
+            productList.replace(m.getKey(),quantity);
+            if (m.getValue()==0){
+                productList.remove(m.getKey());
+            }
+        }
+        return "redirect:/cart";
+    }
+    @GetMapping("/cart")
+    public String carts(@SessionAttribute("cart") Map<Products,Integer> list , Model model){
+        model.addAttribute("cart",list);
+        double total = cartService.getTotal(list);
+        model.addAttribute("total", total);
+        return "/cart";
     }
 }
